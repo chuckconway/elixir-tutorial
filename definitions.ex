d5a486d8384ba9f29e7d5156fa25b91b08094c50
Chuck Conway <charles.conway@libertyhomeequity.com>
2016-07-21 13:43:39 -0700

    first commit

---
+++ definitions.ex
@@ -0,0 +1,20 @@
+#This is a tuple
+tuple = {:label, 1, "Steve", [1,2]}
+
+#This is a list
+list = ["one", 2, 3.00]
+
+#this is a keyword list
+keyword = [one: "one", two:2, three: "three"]
+
+#this is a map
+map = %{key: "value", another_key: "another value"}
+map.key == "value"
+
+struct = %Vehicle{color: :red, wheels: 4}
+
+# this is a comment
+title = "a string"
+price =  12.00
+quantity = 1
+:yellow
